59874760932a6494567fbcf7fb3773970547d9ef
MandorSavall <bogdan.lyamzin.d@gmail.com>
2021-03-14 12:01:17 +0200

    first commit

---
+++ react-infinity-scroll-horizontal/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import ProductList from "./client/Products/components/ProductsList";
+
+function App() {
+  return (
+    <div className="App">
+      <ProductList />
+    </div>
+  );
+}
+
+export default App;
